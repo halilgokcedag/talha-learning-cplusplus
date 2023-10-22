f2652dbc6d49598ec6900c0e85c7287f48cc1d62
Halil Gokcedag <halilhs20@gmail.com>
2023-10-22 14:19:59 +0300

    std eklendi, komutlar kısaltıldı, newline eklendi

--- ortalama.cpp
+++ ortalama.cpp
@@ -1,42 +1,80 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
-
+using namespace std;
 int main() {
     int vize1;
     int final1;
     // Write C++ code here
-    std::cout << "Vize notu giriniz "; // Vize notu istendi
-    std::cin >> vize1; // Vize notu değişkene atandıı
-    std::cout << "Final notu giriniz "; // final notu istedi
-    std::cin >> final1;
+    cout << "Vize notu giriniz "<< endl;; // Vize notu istendi
+    cin >> vize1; // Vize notu değişkene atandıı
+    cout << "Final notu giriniz "<< endl;; // final notu istedi
+    cin >> final1;
     //notlar 0-100 ile arasında mı kontrol et
     if(vize1>100 || final1>100 || vize1<0 || final1<0)
-        std::cout<<"Hata -> Notlardan en az biri hatalı girildi";
+        cout<<"Hata -> Notlardan en az biri hatalı girildi";
     else
     {
         double ortalama=vize1*0.4+final1*0.6;
-        std::cout << ortalama;
+        cout << ortalama << endl;
         
         if(ortalama<=49 && ortalama>0)
-            std::cout << "FF";
+            cout << "FF";
         else if(ortalama<=59 && ortalama>49)
-            std::cout << "FD";
+            cout << "FD";
         else if(ortalama<=64 && ortalama>59)
-            std::cout << "DD";
+            cout << "DD";
         else if(ortalama<=69 && ortalama>64)
-            std::cout << "DC";
+            cout << "DC";
         else if(ortalama<=74 && ortalama>69)
-            std::cout << "CC";
+            cout << "CC";
         else if(ortalama<=79 && ortalama>74)
-            std::cout << "CB"; 
+            cout << "CB"; 
         else if(ortalama<=84 && ortalama>79)
-            std::cout << "BB"; 
+            cout << "BB"; 
         else if(ortalama<=89 && ortalama>84)
-            std::cout << "AB"; 
+            cout << "AB"; 
         else if(ortalama<=100 && ortalama>89)
-            std::cout << "AA"; 
+            cout << "AA"; 
         else
-            std::cout << "Notlar yanlış verildi";
+            cout << "Notlar yanlış verildi";
+        cout << endl;
     }
+    
+    // İkinci dersin notları için
+    cout << "Vize notu giriniz "<< endl;; // Vize notu istendi
+    cin >> vize1; // Vize notu değişkene atandıı
+    cout << "Final notu giriniz "<< endl;; // final notu istedi
+    cin >> final1;
+    //notlar 0-100 ile arasında mı kontrol et
+    if(vize1>100 || final1>100 || vize1<0 || final1<0)
+        cout<<"Hata -> Notlardan en az biri hatalı girildi";
+    else
+    {
+        double ortalama=vize1*0.4+final1*0.6;
+        cout << ortalama << endl;;
+        
+        if(ortalama<=49 && ortalama>0)
+            cout << "FF";
+        else if(ortalama<=59 && ortalama>49)
+            cout << "FD";
+        else if(ortalama<=64 && ortalama>59)
+            cout << "DD";
+        else if(ortalama<=69 && ortalama>64)
+            cout << "DC";
+        else if(ortalama<=74 && ortalama>69)
+            cout << "CC";
+        else if(ortalama<=79 && ortalama>74)
+            cout << "CB"; 
+        else if(ortalama<=84 && ortalama>79)
+            cout << "BB"; 
+        else if(ortalama<=89 && ortalama>84)
+            cout << "AB"; 
+        else if(ortalama<=100 && ortalama>89)
+            cout << "AA"; 
+        else
+            cout << "Notlar yanlış verildi";
+        cout << endl;
+    }
+    
     return 0;
 }
